acb52b0e1ec685292d3dbb7d513fe17742290572
MaslowMae <bmm10025@gmail.com>
2024-03-04 21:41:16 -0500

    update shape files and SVG, looking like a hot mess

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
+const inquirer = require('inquirer');
 import('inquirer')
 const textLength = require('inquirer-maxlength-input-prompt')
 // inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt)
 // import select, { Separator } from '@inquirer/select';
 //npm install @inquirer/select for select seperator option
+//cli takes an object as a map for the arguments you wish to parse.
+//Each property/key in the object is the long version of the argument i.e.
 
 const questions = [
     {   
@@ -20,7 +23,14 @@ const questions = [
         'circle',
         'square', 
         'triangle'
-    ]},
+    ]}
+    //promise chained to pass inquire data to fetch shape.svg
+    .then((res) => fetch(`./lib/more/shapes.js${res.Shape}`))
+    // promises are chained to parse the request for the json data
+    .then((res) => res.Shape.json())
+     // json data is accepted as user and logged to the console
+    .then((answerShape) => console.log(Shape))
+,
 {   
     type: 'maxlength-input',
     name: 'text',
@@ -36,18 +46,17 @@ const questions = [
 
 inquirer.prompt(questions) 
     .then(answers => {
-
-
-
     })
-
-
 
 var text = function() {
 
 }
 
-
+// inquirer.prompt({
+//     type: 'input'
+//     name: ''
+//     message: '',
+// })
 
 
 
